ce19352a0ac4319b4bd8e6161af78596c39b0bb8
WyldWanderer <44330121+WyldWanderer@users.noreply.github.com>
2019-10-14 15:47:22 -0600

    Update tweet_bot.py

--- tweet_bot.py
+++ tweet_bot.py
@@ -1,15 +1,18 @@
+#First step is to apply for developer status with Twitter through developer.twitter.com and generate API keys
+
 import tweepy
 
-CONSUMER_KEY = 'FIUXbox6Xen79Qhx29FUfkiB2'
-CONSUMER_SECRET = 'bgpxM3j8efnXpuo5q24DAjX4OInSpMGwvNF8SOzG1V7tYjNNuN'
-ACCESS_KEY = '1155300186362482690-SarBJ3IG3IaLLzAQd3Am5AzSbj9vjP'
-ACCESS_SECRET = 'pfSWZ8qGQOyKc4wd1ScbapfkVS0U8EsH3ONRmdeuFXJ2b'
+#Copy API keys from developer profile and add to code below
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-#put each user you want to pull the most recent tweet from in the user_name list in the format '@UserName'
+#Put each user you want to pull the most recent tweet from in the user_name list in the format '@UserName'seperated by commas
 user_name = []
 tweetList = []
 oldTweets = []
@@ -19,18 +22,20 @@ def getTweet (user_name):
         user = api.get_user(users)
         tweetInfo = user._json.get('status')
         tweetId = tweetInfo.get('id')
-        tweetList.append(tweetId)
+        if tweetId not in oldTweets:
+            tweetList.append(tweetId)
 
 def retweets (tweetList):
     for tweet in tweetList:
-        if tweet not in oldTweets:
-            api.retweet(tweet)
-            oldTweets.append(tweet)
-    tweetList.clear()        
-           
+        api.retweet(tweet)
+        oldTweets.append(tweet)
+    tweetList.clear()
+    return oldTweets
+
+
 getTweet(user_name)
 retweets(tweetList)
-
+print ('Successful Run')
 
         
         
